d756ff34a3091bd1f2f45a7a001b2c90ca074ced
sargsyansevak <thesevaksargsyan@gmail.com>
2023-07-23 15:25:46 -0500

    aside

--- src/components/Admin/Settings.tsx
+++ src/components/Admin/Settings.tsx
@@ -4,7 +4,7 @@ import { Outlet } from 'react-router-dom'
 
 const Settings = () => {
   return (
-    <div className='w-full h-screen px-10 flex'>
+    <div className='w-full h-screen md:px-4 px-0 flex'>
      <AsideMenu/>
      <div className='content p-6 w-[80%]'>
          <Outlet/>
