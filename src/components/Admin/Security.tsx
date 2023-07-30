dc64350318dcb16ee139d3c78f92c77bc103cd95
sargsyansevak <thesevaksargsyan@gmail.com>
2023-07-30 07:23:52 -0500

    security

--- src/components/Admin/Security.tsx
+++ src/components/Admin/Security.tsx
@@ -78,18 +78,20 @@ const Security = () => {
           </div>
         </div>
       </div>
-      <div className="md:w-1/2 w-full pt-8 pl-4  py-3 sm:flex sm:flex-row-reverse sm:px-6">
+      <div className=" md:w-1/2 w-full  pt-8 pl-4  py-3 flex sm:flex-row-reverse flex-col justify-between sm:px-6 gap-4">
         <button
           type="button"
-          className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm  text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+          // className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-sm  text-white shadow-sm hover:bg-blue-500 sm:ml-3 sm:w-auto"
+          className=" md:w-1/2 w-full rounded-2xl inline-flex justify-center items-center   py-[14px] text-white   bg-[#1c90f3] hover:bg-[#1c8ff3cd] sm:text-sm sm:leading-6 border-none transition-all"
         >
           Պահպանել
         </button>
         <button
           type="button"
-          className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+          // className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm  text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+          className=" md:w-1/2 w-full rounded-2xl inline-flex justify-center items-center   py-[14px] text-gray-900 bg-inherit   hover:bg-black hover:text-white sm:text-sm sm:leading-6 border-2 border-gray-700 transition-all"
         >
-          Չեղարկելել
+          Չեղարկել
         </button>
       </div>
     </div>
