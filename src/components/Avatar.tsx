f3ca9873599b6090e01d7365ef7596cdc05eb8a2
sargsyansevak <thesevaksargsyan@gmail.com>
2023-07-31 05:19:26 -0500

    avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,9 +1,9 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
-
-const Avatar = ({showDropDown}:any) => {
+import { BiLogOut } from "react-icons/bi";
+const Avatar = ({ showDropDown }: any) => {
   const [open, setOpen] = useState<Boolean | null>(false);
-  const ref = useRef<any>()
+  const ref = useRef<any>();
 
   const handleOpen = () => {
     setOpen(!open);
@@ -22,30 +22,36 @@ const Avatar = ({showDropDown}:any) => {
     }
   };
   useEffect(() => {
-    const checkIfClickedOutside = (e:any) => {
+    const checkIfClickedOutside = (e: any) => {
       // If the menu is open and the clicked target is not within the menu,
       // then close the menu
       if (open && ref.current && !ref.current.contains(e.target)) {
-        setOpen(false)
+        setOpen(false);
       }
-    }
+    };
 
-    document.addEventListener("mousedown", checkIfClickedOutside)
+    document.addEventListener("mousedown", checkIfClickedOutside);
 
-   
     return () => {
       // Cleanup the event listener
-      document.removeEventListener("mousedown", checkIfClickedOutside)
-    }
-  }, [open])
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [open]);
   return (
     <div className="reletive z-50" ref={ref}>
-      <div className="w-10 h-10 bg-[#1C90F3] rounded-full cursor-pointer flex justify-center items-center font-bold text-xl text-white" onClick={handleOpen} >
+      <div
+        className="w-10 h-10 bg-[#1C90F3] rounded-full cursor-pointer flex justify-center items-center font-bold text-xl text-white"
+        onClick={handleOpen}
+      >
         <p> {avatarWords(str)}</p>
       </div>
 
       {open && (
-        <div className={`${showDropDown ? 'block' : 'hidden'} absolute z-50 top-[80px] right-[20px]  bg-white divide-y divide-gray-100 rounded-lg  w-44 dark:bg-gray-700 dark:divide-gray-600`} >
+        <div
+          className={`${
+            showDropDown ? "block" : "hidden"
+          } absolute z-50 top-[80px] right-[20px]  bg-white divide-y divide-gray-100 rounded-lg  w-44 dark:bg-gray-700 dark:divide-gray-600`}
+        >
           <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
             <div>{str}</div>
             <div className="font-medium truncate">name@ntluser.com</div>
@@ -62,24 +68,20 @@ const Avatar = ({showDropDown}:any) => {
                 Բեռներ
               </Link>
             </li>
-            <li >
+            <li>
               <Link
-                 to="/admin"
-                 target="_blank"
+                to="/admin"
+                target="_blank"
                 className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
               >
                 Իմ էջը
               </Link>
             </li>
-          
           </ul>
           <div className="py-1">
-            <a
-               href="http://google.com"
-              className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
-            >
-              Դուրս գալ
-            </a>
+            <div className="flex items-center gap-2 cursor-pointer px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">
+              <BiLogOut/>Դուրս գալ
+            </div>
           </div>
         </div>
       )}
