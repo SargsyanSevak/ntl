d2745c4004ca26291804a07abc79cfc063e01237
sargsyansevak <thesevaksargsyan@gmail.com>
2023-08-06 09:52:09 -0500

    loader

--- src/components/Loads.tsx
+++ src/components/Loads.tsx
@@ -1,23 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { testload } from "../data/testload";
 import LoadItem from "./LoadItem";
-import Loader from "./Loader";
+import Loader from "./Loader"; // Replace with your Loader component
 import { LuFilter, LuFilterX } from "react-icons/lu";
 import { RxUpdate } from "react-icons/rx";
 import DashboardFilter from "./DashboardFilter";
 import SortLoads from "../UI/UISort";
+import { LoadProps } from "../interfaces/LoadProps";
+
 const itemsPerRow = 50;
+
 const Loads = () => {
   const [next, setNext] = useState<number>(itemsPerRow);
+  const [isLoading, setIsLoading] = useState(true);
+  const [open, setOpen] = useState<boolean>(false);
+  const [loadData, setLoadData] = useState<LoadProps[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoadData(testload);
+      setIsLoading(false);
+    }, 2000); 
+  }, []);
+
   const handleMoreImage = () => {
     setNext(next + 50);
   };
-  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <>
       <div>
         <div className="w-full h-10 bg-[#eaeaea] dark:bg-[#081423] dark:text-white flex items-center justify-between pl-4 pr-4 ">
-          <p >{testload.length} Total results </p>
+          <p>{loadData.length} Total results</p>
           <div className="h-full flex items-center gap-4 relative -z-1 dark:text-white">
             <SortLoads />
             <div
@@ -28,59 +42,23 @@ const Loads = () => {
             </div>
           </div>
         </div>
-        <div className="lg:block hidden">
-          <div className="w-full h-[30px] bg-gray-300 dark:bg-gray-800 dark:text-gray-200 text-[12px]  flex justify-around gap-[1px] font-semibold text-gray-700">
-            <div className=" flex justify-start items-center pl-[12px] w-[50px] text-[14px] font-bold">
-              <div title={"վերջին թարմացումը"} className="cursor-pointer">
-                <RxUpdate />
-              </div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[50px]">
-              <div title={"բարձման օր"}>օր</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[50px]">
-              <div title={"տրանսպորտային միջոցի տեսակը"}>տ/մ</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[40px]">
-              <div title={"ամբողջական/հավաքական"}>Ա/Հ</div>
-            </div>
-
-            <div className=" flex justify-start items-center pl-[6px] w-[150px]">
-              <div title={"բարձման վայր"}>բարձում</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[35px]">
-              <div></div>
-            </div>
-            <div className="flex justify-start items-center pl-[6px] w-[150px]">
-              <div title={"բերնաթափման վայր"}>բեռնաթափում</div>
-            </div>
-            <div className="flex justify-start items-center pl-[6px] w-[70px]">
-              <div title={"հեռավորություն"}>հեռավ.</div>
-            </div>
-            <div className="flex justify-start items-center pl-[6px] w-[160px]">
-              <div>պատվիրատու</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[140px]">
-              <div>կոնտակտ</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[50px]">
-              <div>ծավալ</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[80px]">
-              <div>քաշ</div>
-            </div>
-            <div className=" flex justify-start items-center pl-[6px] w-[70px]">
-              <div>գին</div>
-            </div>
-            <div className="flex justify-start items-center pl-[6px] w-[70px]"></div>
-          </div>
-        </div>
+        <div className="lg:block hidden"></div>
         <DashboardFilter open={open} />
-        {testload.slice(0, next)?.map((el, i) => (
-          <div key={i} className="pb-[2px]">
-            <LoadItem {...el} />
+
+        {isLoading ? (
+          <div className="w-full h-[calc(100vh-110px)]  flex justify-center items-center">
+            <Loader />
           </div>
-        ))}
+        ) : (
+          <>
+            {loadData.slice(0, next)?.map((el, i) => (
+              <div key={i} className="pb-[2px]">
+                <LoadItem {...el} />
+              </div>
+            ))}
+          </>
+        )}
+
         {next < testload?.length && (
           <div className="w-full flex justify-center py-4 ">
             <button
@@ -91,7 +69,6 @@ const Loads = () => {
             </button>
           </div>
         )}
-        {/* <Loader/> */}
       </div>
     </>
   );
