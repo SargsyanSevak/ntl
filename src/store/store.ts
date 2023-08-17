9c44a4b5430e2c9a2ea1a09461c27fad67f04d0d
AramPetrosyan07 <aspetrosyan@mail.ru>
2023-08-17 18:01:41 +0400

    test3

--- src/store/store.ts
+++ src/store/store.ts
@@ -2,12 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { mapSLice } from "./mapSlice";
 import userSlice from "./userSlice";
 
-// export const rootReducer = configureStore({
-//   reducer: {
-//     taxi: taxiSLice.reducer,
-//   },
-// });
-
 export const store = configureStore({
   reducer: {
     map: mapSLice.reducer,
