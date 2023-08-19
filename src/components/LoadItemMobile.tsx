07ae1e66a91db8b87381809018d5f51f56af5158
sargsyansevak <thesevaksargsyan@gmail.com>
2023-08-19 06:14:05 -0500

    wdwed

--- src/components/LoadItemMobile.tsx
+++ src/components/LoadItemMobile.tsx
@@ -37,7 +37,7 @@ const LoadItemMobile = ({
         className={`relative pb-[50px] dark:border-b-4 border-b-gray-500 lg:hidden flex bg-slate-200 dark:bg-[#0E192D] dark:text-slate-200   border-stone-50 sm:px-4 px-4 text-[0.9rem] md:text-[1.2rem] font-bold`}
       >
         <div
-          className="length text-[12px] absolute  bottom-[22px] right-4  border-[#2183d9] z-50 border-[0.7px]  text-blue-500 dark:text-gray-300 font-bold  px-2 py-2  rounded-xl lg:hidden block"
+          className="length text-[12px] absolute  bottom-[20px] right-4  border-[#2183d9] z-50 border-[0.7px]  text-blue-500 dark:text-gray-300 font-bold  px-2 py-2  rounded-xl lg:hidden block"
           onClick={(e) => {
             e.stopPropagation();
           }}
@@ -47,7 +47,7 @@ const LoadItemMobile = ({
         {boardType === "load" && (
           <Link
           to={`/dashboard/preview/${id}`}
-            className="length  text-[12px] absolute  bottom-[22px] left-4 border-[#2183d9]  z-50 border-[0.7px] text-black dark:text-white px-10 py-2  rounded-xl lg:hidden flex justify-center items-center gap-4 tracking-wider"
+            className="length  text-[12px] absolute  bottom-[20px] left-4 border-[#2183d9]  z-50 border-[0.7px] text-black dark:text-white px-10 py-2  rounded-xl lg:hidden flex justify-center items-center gap-4 tracking-wider"
             onClick={(e) => {
               e.stopPropagation();
             }}
